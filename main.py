59d399b43e1630594cc2365b52017c6d1fbeb240
Николай Александров <energ.vita@mail.ru>
2025-06-08 12:57:58 +0500

    add example

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from src.masks import get_mask_card_number
 
 from src.masks import get_mask_account
+
+from src.widget import mask_account_card
+
+from src.widget import get_date
+
+print("hello word")
