49d0d39db1d499ccf0da87adb512f2afcbf669bb
Николай Александров <energ.vita@mail.ru>
2025-06-08 12:57:58 +0500

    work version add 2 function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from src.masks import get_mask_card_number
 
 from src.masks import get_mask_account
+
+from src.widget import mask_account_card
+
+from src.widget import get_date
--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,11 @@
-from src.masks import get_mask_card_number
-
-from src.masks import get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(user_details: str) -> str:
-    """"Функция, которая проверяет что ввел пользователь(карту или счет) и маскирует"""
-    disguise_user_details = ''
-    number = ''
-    word = ''
+    """ "Функция, которая проверяет что ввел пользователь(карту или счет) и маскирует"""
+    disguise_user_details = ""
+    number = ""
+    word = ""
 
     for symbol in user_details:
         if symbol.isdigit():
@@ -19,6 +17,14 @@ def mask_account_card(user_details: str) -> str:
     else:
         number_mask = get_mask_account(number)
 
-    disguise_user_details = word +' ' + number_mask
+    disguise_user_details = word + " " + number_mask
 
     return disguise_user_details
+
+
+def get_date(current_date: str) -> str:
+    """'Функция, которая показывает дату операции"""
+    data = current_date[:10]
+    data_used = data.split("-")[::-1]
+
+    return ".".join(data_used)
