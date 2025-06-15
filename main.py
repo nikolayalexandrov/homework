f794d2725e23e8cc6d3969702473581303aab8ba
Николай Александров <energ.vita@mail.ru>
2025-06-15 17:47:53 +0500

    add add import func in main

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ from src.masks import get_mask_account
 from src.widget import mask_account_card
 
 from src.widget import get_date
+
+from src.processing import filter_by_state
+
+from src.processing import sort_by_date
