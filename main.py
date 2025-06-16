98da50dfab37ea6573e3244683988b5ad5f03f9b
Николай Александров <energ.vita@mail.ru>
2025-06-16 22:28:46 +0500

    edit README, add run func in main.py

--- main.py
+++ main.py
@@ -11,3 +11,27 @@ from src.widget import get_date
 from src.processing import filter_by_state
 
 from src.processing import sort_by_date
+
+#маскируем номер карты
+card_number = input("ВВедите номере карты: ")
+print(get_mask_card_number(card_number))
+
+#маскируем номер счета
+account = input("ВВедите номере счета: ")
+print(get_mask_account(account))
+
+#маскируем номер карты или счета
+account_card = input("ВВедите номере счета или номер карты: ")
+print(mask_account_card(account_card))
+
+#вывод даты
+date = input("Введите данные: ")
+print(get_date(date))
+
+#фильтруем по значению
+state_use = input("Введите данные: ")
+print(filter_by_state(state_use))
+
+#сортируем по дате
+sort_date = input("Введите данные: ")
+print(sort_by_date(sort_date))
--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(filter_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+from mypy.types import AnyType
+
+
+def filter_by_state(filter_list: list[dict[str, AnyType]], state: str = "EXECUTED") -> list[dict[str, AnyType]]:
     """ "Функция, которая фильтрует список словарей по заданному ключу и значению"""
     value_executed = []
 
@@ -9,6 +12,6 @@ def filter_by_state(filter_list: list[dict], state: str = "EXECUTED") -> list[di
     return value_executed
 
 
-def sort_by_date(sort_date: list[dict]) -> list[dict]:
+def sort_by_date(sort_date: list[dict[str, AnyType]]) -> list[dict[str, AnyType]]:
     """ "Функция, которая сортирует список словарей по дате"""
     return sorted(sort_date, key=lambda x: str(x.get("date")), reverse=True)
