6a0a77d21ed11f4f7a4561205ebe024b5e457e1a
Николай Александров <energ.vita@mail.ru>
2025-07-01 05:00:56 +0300

    add module generators

---
+++ src/generators.py
@@ -0,0 +1,30 @@
+from typing import Iterable
+
+
+def filter_by_currency(transactions: list, currency: str) -> Iterable:
+    for meaning in transactions:
+        if meaning["operationAmount"]["currency"]["code"] == currency:
+            yield meaning
+
+
+def transaction_descriptions(transactions: list) -> Iterable:
+    for operations in transactions:
+       yield operations["description"]
+
+
+def card_number_generator(start = 1, stop = 9999999999999999 ) -> Iterable:
+    """"Функция, которая генерирует номер карты в заданном диапазоне"""
+    for gen_num in range(start, stop):
+        len_numbers = 16 - len(str(gen_num))
+        if len(str(gen_num)) <= 16:
+            card_number_gen = ('0' * len_numbers) + str(gen_num)\
+            card_number = card_number_gen[:4] + " " + card_number_gen[4:8] + " " + card_number_gen[8:12] + " "+ card_number_gen[-4:]
+            yield card_number
+        else:
+            print('Что-то пошло не так')
+            break
+
+
+
+for card_number in card_number_generator(1, 5):
+    print(card_number)
