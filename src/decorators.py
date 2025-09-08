74a1e8c5c59859623e32d74a67f0da1049b32fed
Николай Александров <energ.vita@mail.ru>
2025-09-08 10:37:29 +0500

    add docstring

--- src/decorators.py
+++ src/decorators.py
@@ -2,8 +2,13 @@ import time
 
 
 def log(filename = None):
+    """Декоратор для логирования выполнения функции.
+        Если задан `filename`, лог записывается в файл, иначе выводится в консоль"""
+
     def my_decorator(func):
+        """Возвращает обёртку, которая логирует выполнение функции `func`"""
         def wrapper(*args, **kwargs):
+            """Обёртка для выполнения функции `func` с логированием её результата и ошибок."""
             try:
 
                 result = func(*args, **kwargs)
@@ -13,21 +18,25 @@ def log(filename = None):
                     with open(filename, 'a', encoding="utf-8") as file:
                         file.write(f"Функция {name_func} ок. Результат: {result}")
                         file.close()
-                    return None
+                    #return None
                 else:
                     print(f"Функция {name_func} ок. Результат: {result}")
-                    return None
+                    #return None
             except Exception as e:
+                result = None
                 name_func = func.__name__
                 if filename:
                     file = open(filename, 'a', encoding='utf-8')
                     file.write(f"Ошибка выполнения - {name_func} error: {e}. Inputs: {args}, {kwargs}\n")
                     file.close()
-                    return f"Ошибка выполнения {name_func} error: {e}. Inputs: {args}, {kwargs}\n"
+                    print (f"Ошибка выполнения {name_func} error: {e}. Inputs: {args}, {kwargs}\n")
                 else:
+                    result = None
                     print(f"Ошибка выполнения - {name_func} error: {e}. Inputs: {args}, {kwargs}\n")
+            return result
         return wrapper
     return my_decorator
+
 
 #@log(filename="mylog.txt")
 # @log()
