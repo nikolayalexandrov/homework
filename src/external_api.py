371edd1033e8ae0a84e5e24af454bceae048e216
Николай Александров <energ.vita@mail.ru>
2025-12-07 15:07:01 +0500

    add def pey_transaction redict

--- src/external_api.py
+++ src/external_api.py
@@ -1,23 +1,31 @@
 import os
 import json
+from http.client import responses
+
 from dotenv import load_dotenv
 from utils import load_transactions
 import requests
 
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
-def pay_transactions(pay_transaction: list[dict]) -> float:
+def pay_transactions(load_transaction: dict) -> float:
     """"функцию, которая принимает на вход транзакцию и возвращает сумму транзакции"""
-    for transaction in pay_transaction:
-        if transaction['operationAmount']['currency']['code'] == 'RUB':
-            return transaction['operationAmount']['amount']
-        if transaction['operationAmount']['currency']['code'] != 'RUB':
-            code_amount = transaction['operationAmount']['currency']['code']
-            sum_amount = transaction['operationAmount']['amount']
-            from_conv_amount = 'RUB'
-            conv_amount = url = f"https://api.apilayer.com/exchangerates_data/convert?to={from_conv_amount}&from={code_amount}&amount={sum_amount}"
+    try:
+        for transaction in load_transaction:
+            if transaction['operationAmount']['currency']['code'] == 'RUB':
+             print(transaction['operationAmount']['amount'])
+            if transaction['operationAmount']['currency']['code'] != 'RUB':
+                code_amount = transaction['operationAmount']['currency']['code']
+                sum_amount = transaction['operationAmount']['amount']
+                from_conv_amount = 'RUB'
+                conv_amount = f"https://api.apilayer.com/exchangerates_data/convert?to={from_conv_amount}&from={code_amount}&amount={sum_amount}"
             #payload = date.today()
-            headers = {
-                "apikey": "API_KEY"
-            }
+                headers = {
+                    "apikey": "API_KEY"
+                }
 
-            response = requests.request("GET", conv_amount, headers=headers)
+                response = requests.request("GET", conv_amount, headers=headers)
+        return response
+    except Exception as e:
+        print (e)
--- src/utils.py
+++ src/utils.py
@@ -4,10 +4,12 @@ import json
 def load_transactions(file_path):
     """"функциz, которая принимает на вход путь до JSON-файла и возвращает список
     словарей с данными о финансовых транзакциях."""
-    with open(file_path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
 
         return data
-
+    except Exception as e:
+        print (e)
 #print(load_transactions('C:\\Users\\energ\\OneDrive\\Рабочий стол\\SkyPro\\PythonProject\\data\\operations.json'))
 
